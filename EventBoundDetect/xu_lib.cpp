0a689b5e991cfb4aaa3dc8f69bed559c08610b8a
Xu Shen <shenxuustc@gmail.com>
2015-10-31 13:01:59 +0800

    add my own lib file

---
+++ EventBoundDetect/xu_lib.cpp
@@ -0,0 +1,4 @@
+// this is the lib file created by xu shen in 10/31/2015
+// contact: shenxu@mail.ustc.edu.cn
+
+#include "xu_lib.h"
---
+++ EventBoundDetect/xu_lib.h
@@ -0,0 +1,5 @@
+// this is the lib file created by xu shen in 10/31/2015
+// contact: shenxu@mail.ustc.edu.cn
+
+#include <stdio.h>
+
